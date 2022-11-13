eb3e43873c2f48f7a043475d34afa4d59e7957c5
Mo Mobarak <mo@mobarak.com>
2022-11-13 13:51:07 +0000

    MSB example

--- util.py
+++ util.py
@@ -72,7 +72,7 @@ def extract_message_from_image(image):
                     return message
                     
     return message
-  
+
 def show_images(images):
     fig = plt.figure(figsize=(20, 7))
     rows = 1
@@ -83,3 +83,55 @@ def show_images(images):
         plt.imshow(image[0])
         plt.axis('off')
         plt.title(image[1])
+
+        
+def hide_message_in_image_MSB(message_string, cover_image):
+    encoded_image = cover_image.copy()
+    
+    message_bytes = string_as_byte_array(message_string)
+    message_bits = byte_array_as_bit_array(message_bytes)
+    max_bit = len(message_bits) - 1
+    bit = 0
+    
+    nrows, ncolumns, ncolors = cover_image.shape
+    for row in range(nrows):
+        for column in range(ncolumns):
+            for color in range(ncolors):
+                if bit > max_bit:
+                    # TODO - mark end of message
+                    return encoded_image
+                
+                message_bit = message_bits[bit]
+                color_byte = encoded_image[row][column][color]
+                color_byte &= 0x7F
+                color_byte |= message_bit << 7
+                encoded_image[row][column][color] = color_byte
+                bit += 1
+    
+    return encoded_image
+        
+def extract_message_from_image_MSB(image):
+    message = ""
+    message_byte = 0
+    bits_in_byte = 0
+  
+    nrows, ncolumns, ncolors = image.shape
+    for row in range(nrows):
+        for column in range(ncolumns):
+            for color in range(ncolors):
+                message_bit = (image[row][column][color] >> 7) & 0x01
+                message_byte |= message_bit
+                bits_in_byte += 1
+                
+                if bits_in_byte == 8:
+                    message += chr(message_byte)
+                    message_byte = 0
+                    bits_in_byte = 0
+                else:
+                    message_byte <<= 1
+                    
+                # TODO - detect end of message
+                if len(message) > 1000:
+                    return message
+                    
+    return message
